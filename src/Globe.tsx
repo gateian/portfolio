dd584bdfed29ec7aacf04b44f9134d3463917367
Ian Hamblin <ian@3deo.com>
2024-08-05 17:30:48 +0100

    Added test banner.

--- src/Globe.tsx
+++ src/Globe.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Viewer, Entity, Camera, CameraFlyTo } from "resium";
 import { Cartesian3 } from "cesium";
 import styled from "@emotion/styled";
+import { Overlay, HeroBanner } from "./StyledComponents";
 
 const GlobeContainer = styled.div`
   width: 100%;
@@ -28,6 +29,12 @@ const Globe: React.FC = () => {
           destination={Cartesian3.fromDegrees(-74.006, 40.7128, 1000)}
         />
       </StyledViewer>
+      <Overlay>
+        <HeroBanner>
+          <h1>Ian K Hamblin</h1>
+          <p>3D Specialist | Realtime Developer</p>
+        </HeroBanner>
+      </Overlay>
     </GlobeContainer>
   );
 };
