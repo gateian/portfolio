373066ea02df42ec4aa94c5b00ea7642fd8d594d
Ian Hamblin <ian@3deo.com>
2024-08-06 10:35:38 +0100

    Tweaked lighting.

--- src/ThreeScene.tsx
+++ src/ThreeScene.tsx
@@ -50,10 +50,10 @@ function BoxGrid({ width, depth }: BoxGridProps) {
 
 export default function ThreeScene() {
   return (
-    <Canvas camera={{ position: [10, 10, 10], fov: 50 }}>
-      <ambientLight intensity={0.5} />
+    <Canvas camera={{ position: [10, 10, 10], fov: 50 }} shadows>
+      <ambientLight intensity={0.8} />
       <directionalLight
-        position={[-5, 5, 5]}
+        position={[-3, 5, 5]}
         intensity={1}
         castShadow
         shadow-mapSize-width={2048}
