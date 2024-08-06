4d83f1f1e128c1800e0573798cded787258a1714
Ian Hamblin <ian@3deo.com>
2024-08-06 10:19:14 +0100

    Added box grid.

--- src/ThreeScene.tsx
+++ src/ThreeScene.tsx
@@ -1,26 +1,59 @@
-import { useRef } from "react";
+import { useMemo, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
+import * as THREE from "three";
+import { FlyControls, Stats } from "@react-three/drei";
 
-function Box(props: any) {
-  const mesh = useRef<Mesh>(null!);
-  useFrame((_state, delta) => (mesh.current.rotation.x += delta));
+interface BoxGridProps {
+  width: number;
+  depth: number;
+}
+
+function BoxGrid({ width, depth }: BoxGridProps) {
+  const instancedMeshRef = useRef<THREE.InstancedMesh>(null!);
+  const tempObject = useMemo(() => new THREE.Object3D(), []);
+  const count = width * depth;
+
+  const yScales = useMemo(() => {
+    return Array.from({ length: count }, () => Math.random() * 2 + 0.5); // Random height between 0.5 and 2.5
+  }, [count]);
+
+  useFrame((state, delta) => {
+    let i = 0;
+    for (let x = 0; x < width; x++) {
+      for (let z = 0; z < depth; z++) {
+        const id = i++;
+        tempObject.position.set(
+          x - width / 2 + 0.5,
+          yScales[id] / 2,
+          z - depth / 2 + 0.5
+        );
+        tempObject.scale.set(1, yScales[id], 1);
+        tempObject.updateMatrix();
+        instancedMeshRef.current.setMatrixAt(id, tempObject.matrix);
+      }
+    }
+    instancedMeshRef.current.instanceMatrix.needsUpdate = true;
+  });
+
   return (
-    <mesh {...props} ref={mesh}>
+    <instancedMesh ref={instancedMeshRef} args={[undefined, undefined, count]}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"orange"} />
-    </mesh>
+      <meshPhongMaterial color="purple" />
+    </instancedMesh>
   );
 }
 
 export default function ThreeScene() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [10, 10, 10], fov: 50 }}>
       <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+      <pointLight position={[10, 10, 10]} />
+      <BoxGrid width={10} depth={10} />
+      <gridHelper args={[10, 10]} />
+      <axesHelper args={[5]} />
+      <FlyControls movementSpeed={10} rollSpeed={0.5} dragToLook={true} />
+      <Stats />
     </Canvas>
   );
 }
