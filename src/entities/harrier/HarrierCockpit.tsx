c3dbb94805d3471a583c853190063473bdab5dfc
Ian Hamblin <ian@3deo.com>
2024-10-16 12:51:20 +0100

    Refactored routing to load 3d content better.

--- src/entities/harrier/HarrierCockpit.tsx
+++ src/entities/harrier/HarrierCockpit.tsx
@@ -1,17 +1,8 @@
 import { useGLTF } from "@react-three/drei";
-import { useLocation } from "react-router-dom";
 
 const HarrierCockpit = () => {
   const { scene } = useGLTF("./3d/HarrierPortfolioExport.glb");
 
-  const location = useLocation();
-
-  if (location.pathname !== "/combat") {
-    console.log("Not rendering Harrier Cockpit");
-    return null;
-  }
-
-  console.log("Rendering Harrier Cockpit");
   return (
     <primitive
       object={scene}
